675abd179e6976dbaefd9dad270a3e820113f975
Eric Korth <ebkorth7@gmail.com>
2022-01-07 10:46:31 -0500

    Added checks for different calculations and special characters. Added negative button and finalized styles. 1-7-2022

--- components/calculatorMain.js
+++ components/calculatorMain.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { DIGITS } from '../shared/digits';
-import { StyleSheet, SafeAreaView, View, Text, Button } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
 
 
 export default class Calculator extends Component {
@@ -15,7 +15,8 @@ export default class Calculator extends Component {
             subtractStylesActive: false,
             multiplyStylesActive: false,
             divideStylesActive: false,
-            setCalc: ''
+            setCalc: '',
+            result: ''
         }   
     }
     
@@ -23,9 +24,15 @@ export default class Calculator extends Component {
 
 
         const updateCalc = (value) => {
-            this.setState({
-                setCalc: this.state.setCalc + value
-            });
+            if(this.state.result === '') {
+                this.setState({
+                    setCalc: this.state.setCalc + value
+                });
+            } else {
+                this.setState({
+                    setCalc: this.state.setCalc
+                })
+            }
 
             if(this.state.setCalc.length === 9) {
                 this.setState({
@@ -42,8 +49,27 @@ export default class Calculator extends Component {
                 subtractStylesActive: false,
                 multiplyStylesActive: false,
                 divideStylesActive: false,
-                setCalc: ''
+                setCalc: '',
+                result: ''
             });
+        }
+
+        const handleDecimal = (value) => {
+            if(this.state.setCalc.includes('.') && value === '.') {
+                return;
+            } else {
+                updateCalc(value);
+            }
+        }
+
+        const handleNegative = (value) => {
+            if(this.state.setCalc.includes('-') && value === '-') {
+                return;
+            } else if(this.state.setCalc && value === '-') {
+                return;
+            } else {
+                updateCalc(value);
+            }
         }
 
         const handleOperator = (value) => {
@@ -53,28 +79,38 @@ export default class Calculator extends Component {
                 setCalc: ''
             });
 
+            if(this.state.setCalc === '') {
+                this.setState({
+                    input: '0'
+                })
+            }
+
             switch(value) {
                 case '+':
                     this.setState({
-                        addStylesActive: true
+                        addStylesActive: true,
+                        result: ''
                     })
                     break;
 
                 case '-':
                     this.setState({
-                        subtractStylesActive: true
+                        subtractStylesActive: true,
+                        result: ''
                     })
                     break;
 
                 case '*':
                     this.setState({
-                        multiplyStylesActive: true
+                        multiplyStylesActive: true,
+                        result: ''
                     })
                     break;
                 
                 case '/':
                     this.setState({
-                        divideStylesActive: true
+                        divideStylesActive: true,
+                        result: ''
                     })
                     break;
             }
@@ -87,12 +123,14 @@ export default class Calculator extends Component {
                     if(parseFloat(this.state.input) + parseFloat(this.state.setCalc) > 999999999) {
                         this.setState({
                             setCalc: 'E',
-                            addStylesActive: false
+                            addStylesActive: false,
+                            result: this.state.setCalc
                         })
                     } else {
                         this.setState({
                             setCalc: parseFloat(this.state.input) + parseFloat(this.state.setCalc),
-                            addStylesActive: false
+                            addStylesActive: false,
+                            result: this.state.setCalc
                         })
                     }
                     break;
@@ -100,7 +138,8 @@ export default class Calculator extends Component {
                 case '-':
                     this.setState({
                         setCalc: parseFloat(this.state.input) - parseFloat(this.state.setCalc),
-                        subtractStylesActive: false
+                        subtractStylesActive: false,
+                        result: this.state.setCalc
                     })
                     break;
 
@@ -108,12 +147,14 @@ export default class Calculator extends Component {
                     if(parseFloat(this.state.input) * parseFloat(this.state.setCalc) > 999999999) {
                         this.setState({
                             setCalc: 'E',
-                            multiplyStylesActive: false
+                            multiplyStylesActive: false,
+                            result: this.state.setCalc
                         })
                     } else {
                         this.setState({
                             setCalc: parseFloat(this.state.input) * parseFloat(this.state.setCalc),
-                            multiplyStylesActive: false
+                            multiplyStylesActive: false,
+                            result: this.state.setCalc
                         })
                     }
                     break;
@@ -122,12 +163,14 @@ export default class Calculator extends Component {
                     if(parseFloat(this.state.input) / parseFloat(this.state.setCalc) > 999999999) {
                         this.setState({
                             setCalc: 'E',
-                            divideStylesActive: false
+                            divideStylesActive: false,
+                            result: this.state.setCalc
                         })
                     } else {
                         this.setState({
                             setCalc: parseFloat(this.state.input) / parseFloat(this.state.setCalc),
-                            divideStylesActive: false
+                            divideStylesActive: false,
+                            result: this.state.setCalc
                         })
                     }
                     break;
@@ -142,76 +185,86 @@ export default class Calculator extends Component {
                     </Text>
                 </View>
                 <View style={styles.operatorContainer} className='operators'>
-                    <View style={styles.operator}>
-                        <Button
-                            className={`col-2 button operator${this.state.addStylesActive ? 'Active' : ''}`}
-                            key='12'
-                            value="+" 
-                            color='#fff'
-                            onPress={() => handleOperator("+")}
-                            title='+'>
-                            +
-                        </Button>
-                    </View>
-                    <View style={styles.operator}>
-                        <Button
-                            className={`col-2 button operator${this.state.subtractStylesActive ? 'Active' : ''}`}
-                            value="-"
-                            key='13'
-                            color='#fff'
-                            onPress={() => handleOperator("-")}
-                            title='-'>
-                            -
-                        </Button>
-                    </View>
-                    <View style={styles.operator}>
-                        <Button
-                            className={`col-2 button operator${this.state.multiplyStylesActive ? 'Active' : ''}`}
-                            value="*"
-                            key='14'
-                            color='#fff'
-                            onPress={() => handleOperator("*")}
-                            title='*'>
-                            *
-                        </Button>
-                    </View>
-                    <View style={styles.operator}>
-                        <Button
-                            className={`col-2 button operator${this.state.divideStylesActive ? 'Active' : ''}`}
-                            value="/"
-                            key='15'
-                            color='#fff'
-                            onPress={() => handleOperator("/")}
-                            title='/'>
-                            /
-                        </Button>
-                    </View>
-                    <View style={styles.operator}>
-                        <Button
-                            className='col-4 button operator'
-                            value='C'
-                            key='16'
-                            color='#fff'
-                            onPress={() => resetCalc()}
-                            title='C'>
-                            C
-                        </Button>
-                    </View>
+                    <TouchableOpacity
+                        style={styles.operatorTopLeft}
+                        value='C'
+                        key='16'
+                        onPress={() => resetCalc()}
+                    >
+                        <View>
+                            <Text style={styles.operatorTextStyles}>C</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.operator}
+                        key='12'
+                        value="+" 
+                        onPress={() => handleOperator("+")}
+                    >
+                        <View>
+                            <Text style={styles.operatorTextStyles}>+</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.operator}
+                        value="-"
+                        key='13'
+                        onPress={() => handleOperator("-")}
+                    >
+                        <View>
+                            <Text style={styles.operatorTextStyles}>-</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.operator}
+                        value="*"
+                        key='14'
+                        onPress={() => handleOperator("*")}
+                    >
+                        <View>
+                            <Text style={styles.operatorTextStyles}>*</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.operatorTopRight}
+                        value="/"
+                        key='15'
+                        onPress={() => handleOperator("/")}
+                    >
+                        <View>
+                            <Text style={styles.operatorTextStyles}>/</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
-                <View style={styles.buttonContainer} className="row">
+
+                <View style={styles.buttonContainer}>
                 {this.state.digits.map(a =>
-                    <View style={styles.button} className="digits">
-                        <Button color='purple' className='col-4 button' key={a} value={a} title={a.toString()} onPress={() => updateCalc(a.toString())}>{a}</Button>
-                    </View>)}
-                    <View style={styles.button}>
-                        <Button className='col-4 button' color='purple' style={{borderBottomLeftRadius: '10px'}} key='0' value='0' title='0' onPress={() => updateCalc('0')}>0</Button>
-                    </View>
-                    <View style={styles.button}>
-                        <Button className='col-4 button' key='10' value='.' title='.' color='purple' onPress={() => updateCalc('.')}>.</Button>
-                    </View>
-                    <View style={styles.operatorEqualSign}>
-                        <Button className='col-4 button operator' color='#fff' key='11' value='=' title='=' onPress={() => solve(this.state.operator)}>=</Button>
-                    </View>
+                    <TouchableOpacity style={styles.button} key={a} value={a} onPress={() => updateCalc(a.toString())}>
+                        <View>
+                            <Text style={styles.buttonTextStyles}>{a}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+                    <TouchableOpacity style={styles.button} key='0' value='0' onPress={() => updateCalc('0')}>
+                        <View>
+                            <Text style={styles.buttonTextStyles}>0</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} key='17' value='.' onPress={() => handleDecimal('.')}>
+                        <View>
+                            <Text style={styles.buttonTextStyles}>.</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} key='10' value='-' onPress={() => handleNegative('-')}>
+                        <View>
+                            <Text style={styles.buttonTextStyles}>-</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.operatorEqualSign} key='11' value='=' onPress={() => solve(this.state.operator)}>
+                        <View>
+                            <Text style={styles.operatorTextStyles}>=</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </SafeAreaView>
         );
@@ -221,44 +274,78 @@ export default class Calculator extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width: '100%',
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#11000F'
     },
     display: {
-        padding: 10
+        padding: 15
     },
     displayText: {
         color: '#fff',
-        fontSize: 40
+        fontSize: 60
     },
     button: {
-        borderRadius: 0,
-        fontSize: 20,
-        backgroundColor: '#fff',
-        width: '33.2%'
+        backgroundColor: '#D3D3D3',
+        width: '33.2%',
+        height:'27%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: .5
     },
     buttonContainer: {
         flexWrap: 'wrap',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        height:'57%'
+    },
+    buttonTextStyles: {
+        fontSize: 35
     },
     operator: {
-        borderRadius: 0,
-        fontSize: 20,
         backgroundColor: 'purple',
         color: '#fff',
-        width: '20%'
+        width: '20%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: .5
     },
-    operatorEqualSign: {
-        borderRadius: 0,
-        fontSize: 20,
+    operatorTopLeft: {
         backgroundColor: 'purple',
         color: '#fff',
-        width: '33.2%'
+        width: '20%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: .5,
+        borderTopLeftRadius: 20
+    },
+    operatorTopRight: {
+        backgroundColor: 'purple',
+        color: '#fff',
+        width: '20%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: .5,
+        borderTopRightRadius: 20
+    },  
+    operatorEqualSign: {
+        fontSize: 20,
+        backgroundColor: 'purple',
+        width: '100%',
+        height: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomRightRadius: 20,
+        borderBottomLeftRadius: 20
+    },
+    operatorTextStyles: {
+        fontSize: 35,
+        color: '#fff'
     },
     operatorContainer: {
         flexWrap: 'wrap',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        height: '15%'
     }
 })
